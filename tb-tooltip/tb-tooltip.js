177e3737b70899f9c759353869d137cfb976db8b
Artem Andreev <andreev.artem@gmail.com>
2012-06-12 12:41:09 +0400

    tb-tooltip: directive for twitter bootstrap tooltip

---
+++ tb-tooltip/tb-tooltip.js
@@ -0,0 +1,11 @@
+angular.module("ExperimentsModule", [])
+    .directive("tbTooltip", function($interpolate){
+                return function(scope, element, iAttrs) {
+                        iAttrs.$observe('title', function(value) {
+                            // for updating tooltip when title changed
+                            element.removeData('tooltip');
+                            
+                            element.tooltip();
+                    });
+                }
+    });
