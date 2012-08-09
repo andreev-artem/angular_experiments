832837b9113da709a2d89c1f35c01fb8720d8609
Artem Andreev <andreev.artem@gmail.com>
2012-08-09 14:31:16 +0400

    First not very working implementation

--- ui-datetime/ui-datetime.js
+++ ui-datetime/ui-datetime.js
@@ -4,10 +4,42 @@ angular.module("ExperimentsModule", [])
             restrict: 'EA',
             replace: true,
             template: '<div class="ui-datetime">' +
-                        '<input type="text" ng-model="_date">' +
-                        '<input type="number" ng-model="_hour" min="0" max="23">' +
-                        '<input type="number" ng-model="_minute" min="0" max="59">' +
+                        '<input type="text" ng-model="_date" class="date">' +
+                        '<input type="number" ng-model="_hours" min="0" max="23" class="hours">' +
+                        '<input type="number" ng-model="_minutes" min="0" max="59" class="minutes">' +
                       '</div>',
-            scope: true
+            require: 'ngModel',
+            scope: true,
+            link: function(scope, element, attrs, ngModelCtrl) {
+                var elDate = element.find('input.date');
+
+                ngModelCtrl.$render = function () {
+                    var date = new Date(ngModelCtrl.$viewValue);
+                    var fillNull = function(num) {
+                        if (num < 10) return '0' + num;
+                        return num;
+                    };
+                    scope._date = fillNull(date.getDate() + 1) + '.' + fillNull(date.getMonth() + 1) + '.' + date.getFullYear();
+                    scope._hours = date.getHours();
+                    scope._minutes = date.getMinutes();
+                };
+
+                elDate.datepicker({
+                    dateFormat: 'dd.mm.yy',
+                    onSelect: function(value, picker) {
+                        scope._date = value;
+                        scope.$apply();
+                    }
+                });
+
+                var watchExpr = function() {
+                    var res = scope.$eval('_date').split('.');
+                    if (res.length == 3) return new Date(res[2], res[1] - 1, res[0], scope.$eval('_hours'), scope.$eval('_minutes')).getTime();
+                    return 0;
+                };
+                scope.$watch(watchExpr, function (newValue) {
+                    ngModelCtrl.$setViewValue(new Date(newValue));
+                });
+            }
         };
     });
