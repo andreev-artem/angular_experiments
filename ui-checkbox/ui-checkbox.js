8b9c790adbd5c554e921e474152219be87f2ede7
Artem Andreev <andreev.artem@gmail.com>
2013-01-22 18:38:56 +0400

    ui-checkbox improved using transclude

--- ui-checkbox/ui-checkbox.js
+++ ui-checkbox/ui-checkbox.js
@@ -3,16 +3,16 @@ angular.module("ExperimentsModule", [])
         return {
             restrict: 'EA',
             replace: true,
+            transclude: true,
             template:
                 '<div class="checkbox-control" ng-click="toggle()">' +
                     '<span ng-class="{\'icon-check-empty\': !value, \'icon-check\': value}"></span>' +
-                    '<span class="checkbox-label">{{label}}</span>' +
+                    '<span class="checkbox-label" ng-transclude></span>' +
                 '</div>',
             require: 'ngModel',
             scope: true,
             link: function (scope, element, attrs, ngModelCtrl) {
                 scope.value = false;
-                scope.label = attrs.label;
 
                 ngModelCtrl.$render = function () {
                     scope.value = ngModelCtrl.$viewValue;
